5f5ae8c0a0365d4873d739cec06bde2d379a718a
zhoubin1986 <985619620@qq.com>
2022-06-29 10:13:04 +0800

    7

--- Day05/src/cn/demo/static1/StaticDemo6.java
+++ Day05/src/cn/demo/static1/StaticDemo6.java
@@ -2,8 +2,12 @@ package cn.demo.static1;
 
 public class StaticDemo6 {
 	public static void main(String[] args) {
-		Son s = new Son();
-		s.m();
+		Farther f = new Son();   //向上造型
+		f.m();  /*虽然f指向的是一个子类对象，但是依然调用的是父类的类方法。
+			        甚至在f指向一个空对象null的时候，也能够成功调用m().
+			        所以和f指向哪个对象无关，只和f的类型有关系。
+			        当然，也不建议通过对象去调用类方法，而应该直接通过类去调用类方法，从而规避理解上的歧义。 */
+		Son.m();
 	}
 }
 
