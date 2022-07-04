4ae3d2e4f4cc7212f2a78f21d9125484c9d9e3b9
zhoubin1986 <985619620@qq.com>
2022-07-04 22:08:41 +0800

    modified Day06

--- Day06/src/cn/zhou/abstrackx/AbstrackDemo.java
+++ Day06/src/cn/zhou/abstrackx/AbstrackDemo.java
@@ -10,37 +10,35 @@ public class AbstrackDemo {
 abstract class Animal {
 	String color;
 	double weight;
-	
-	
-	public abstract void eat();  //省掉了语句块
-	
+
+	public abstract void eat(); // 省掉了语句块
+
 	public void sleep() {
 		System.out.println("Animal-->sleep");
 	}
 }
 
 abstract class Pet {
-	private Pet() {  
-		
+	private Pet() {
+
 	}
-	
-	//内部类，是可见的
+
+	// 内部类，是可见的
 	class Pet_Son extends Pet {
-		
+
 	}
 }
 
-/*class Pet_Son extends Pet {
-	public Pet_Son() {
-		super(); //默认
-	}
-}*/
+/*
+ * class Pet_Son extends Pet { public Pet_Son() { super(); //默认 } }
+ */
 
 class Dog extends Animal {
 	@Override
 	public void eat() {
 		System.out.println("Dog-->eat");
 	}
+
 	public void sleep() {
 		System.out.println("Dog-->sleep");
 	}
@@ -50,6 +48,7 @@ class Cat extends Animal {
 	public void eat() {
 		System.out.println("Cat-->eat");
 	}
+
 	public void sleep() {
 		System.out.println("Cat-->sleep");
 	}
@@ -58,7 +57,7 @@ class Cat extends Animal {
 class Pig extends Animal {
 	@Override
 	public void eat() {
-		
-	}  
-	
+
+	}
+
 }
