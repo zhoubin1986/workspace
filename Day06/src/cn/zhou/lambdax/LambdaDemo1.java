0516a37de86ad188eca7e88f7f26d461b5375396
zhoubin1986 <985619620@qq.com>
2022-07-06 14:23:45 +0800

    package

--- Day06/src/cn/zhou/lambdax/LambdaDemo1.java
+++ Day06/src/cn/zhou/lambdax/LambdaDemo1.java
@@ -1,7 +1,24 @@
 package cn.zhou.lambdax;
 
 public class LambdaDemo1 {
-	
+	public static void main(String[] args) {
+		/*Factory f = new Factory() {
+			@Override
+			public Clothes makeClother(String color, double price) {
+				return new Clothes(color,price);
+			}
+		};*/
+		
+//		Factory f = (color,price) -> new Clothes(color,price);
+		//方法体只有一句，直接调用了已有类中的构造方法
+		Factory f = Clothes::new;
+		Clothes c = f.makeClother("RED", 1.0);
+		System.out.println(c.getColor());
+	}
+}
+
+interface Factory {
+	Clothes makeClother(String color,double price);
 }
 
 class Clothes {
