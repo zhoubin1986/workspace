34d4d05543c2a94d1467b9d4eb66c1f2520fb461
zhoubin1986 <985619620@qq.com>
2022-07-11 17:06:04 +0800

    Sring_RegEx

--- Day07/src/cn/zhou/stringx/RegularExpressionDemo.java
+++ Day07/src/cn/zhou/stringx/RegularExpressionDemo.java
@@ -33,6 +33,7 @@ public class RegularExpressionDemo {
 //		System.out.println(str.matches(".")); //.表示任意一个字符
 //		System.out.println(str.matches("\\.")); //java提供的转义*.文件
 		System.out.println(str1.matches("\\")); //java提供的转义*.文件
+		System.out.println(str1.matches("\\w"));
 		
 	}
 }
