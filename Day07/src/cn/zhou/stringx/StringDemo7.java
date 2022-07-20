bcf41c11001f2c0c4c073cf011a1dc0bbdd7cba1
zhoubin1986 <985619620@qq.com>
2022-07-20 16:58:51 +0800

    1

---
+++ Day07/src/cn/zhou/stringx/StringDemo7.java
@@ -0,0 +1,10 @@
+package cn.zhou.stringx;
+
+public class StringDemo7 {
+	public static void main(String[] args) {
+		String str = "";
+		//ÅÐ¶Ï×Ö·û´®ÖÐÊÇ·ñº¬ÓÐ"ab"
+		str.contains("ab");
+		str.matches(".*ab.*");
+	}
+}
