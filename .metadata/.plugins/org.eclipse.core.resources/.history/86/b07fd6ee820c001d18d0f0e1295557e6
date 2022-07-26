66c5298f0270bee1c285e4b80d5c230b11bbf50b
zhoubin1986 <985619620@qq.com>
2022-07-26 17:05:43 +0800

    1

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/86/b07fd6ee820c001d18d0f0e1295557e6
@@ -0,0 +1,10 @@
+package cn.zhou.stringx;
+
+import java.util.Scanner;
+
+public class RegExReplace2 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String str = s.nextLine();
+	}
+}
--- Day07/src/cn/zhou/stringx/RegExReplace2.java
+++ Day07/src/cn/zhou/stringx/RegExReplace2.java
@@ -4,7 +4,15 @@ import java.util.Scanner;
 
 public class RegExReplace2 {
 	public static void main(String[] args) {
+		//输入一个字符串，统计这个字符串中每一个字符出现的个数
 		Scanner s = new Scanner(System.in);
 		String str = s.nextLine();
+		while(str.length() != 0) {
+			int len = str.length();
+			char ch = str.charAt(0);
+			str = str.replaceAll(ch + "", "");
+			int num = len - str.length();
+			System.out.println(ch + ":" + num);
+		}
 	}
 }
